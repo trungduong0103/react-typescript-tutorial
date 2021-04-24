05d981b480ea0cf3f448b6221a68f2377f9d5797
trungduong0103 <trungduong0103@gmail.com>
2021-04-24 22:57:57 +0700

    render prop typing

---
+++ src/tutorials/ben-awad/Counter.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+interface CounterProps {
+  children: (
+    count: number,
+    setCount: React.Dispatch<React.SetStateAction<number>>
+  ) => JSX.Element | null;
+}
+
+const Counter: React.FC<CounterProps> = ({ children }) => {
+  const [count, setCount] = useState(0);
+  return <div>{children(count, setCount)}</div>;
+};
+
+export default Counter;
--- src/tutorials/ben-awad/index.tsx
+++ src/tutorials/ben-awad/index.tsx
@@ -1,13 +1,24 @@
 // props
+import Counter from "./Counter";
 import TextField from "./TextField";
 
 const BenAwad = () => {
   return (
-    <TextField
-      person={{ firstname: "Trung", lastname: "Duong" }}
-      text="Hello"
-      
-    />
+    <>
+      <TextField
+        person={{ firstname: "Trung", lastname: "Duong" }}
+        text="Hello"
+        handleChange={() => {}}
+      />
+      <Counter>
+        {(count, setCount) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )}
+      </Counter>
+    </>
   );
 };
 
